ca58de38371b719b77bec2f16bc0a9e82f478879
Yash <52369876+Yash10bloodstorm@users.noreply.github.com>
2019-12-08 20:09:49 +0530

    .

--- Task_3/GUI.py
+++ Task_3/GUI.py
@@ -1,8 +1,24 @@
 from tkinter import *
+import webbrowser
+
+def openCodeIn():
+    url_1 ='https://codein.withgoogle.com/dashboard/'
+    webbrowser.open_new_tab(url_1)
 
 root = Tk()
+topFrame = Frame(root)
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
 
+button_1 = Button(topFrame, text='Open', fg='Purple',background='grey', activebackground='black', command=openCodeIn)
+button_2 = Button(topFrame, text='Button_2', fg='blue')
+button_3 = Button(bottomFrame, text='Button_3', fg='green')
+button_4 = Button(bottomFrame, text='Button_4', fg='yellow')
 
-
+button_1.pack(side=LEFT)
+button_2.pack(side=RIGHT)
+button_3.pack(side=LEFT)
+button_4.pack(side=RIGHT)
 
 root.mainloop()
