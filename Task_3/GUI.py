d4ff5cfe684a319c1a65995cd91fb7565d7571a4
Yash <52369876+Yash10bloodstorm@users.noreply.github.com>
2019-12-09 09:45:52 +0530

    .

--- Task_3/GUI.py
+++ Task_3/GUI.py
@@ -1,24 +1,53 @@
 from tkinter import *
 import webbrowser
+import tkinter.ttk as ttk
+from tkinter.filedialog import askopenfile
+from tkinter import filedialog
+import os
+import subprocess
+
+
 
 def openCodeIn():
     url_1 ='https://codein.withgoogle.com/dashboard/'
     webbrowser.open_new_tab(url_1)
-
+def openFinder():
+    rep = filedialog.askopenfilenames(
+    	parent=root,
+    	initialdir='/',
+    	initialfile='Macintosh HD',
+    	filetypes=[
+    		("All files", "*"),
+    		("JPEG", "*.jpg"),
+    		("All files", "*")])
+def openGitHubDesk():
+	subprocess.call('/Applications/GitHub Desktop.app')
+def openVsCode():
+    subprocess.call('/Applications/Visual Studio Code.app')
+   
+    
 root = Tk()
-topFrame = Frame(root)
-topFrame.pack()
-bottomFrame = Frame(root)
-bottomFrame.pack(side=BOTTOM)
 
-button_1 = Button(topFrame, text='Open', fg='Purple',background='grey', activebackground='black', command=openCodeIn)
-button_2 = Button(topFrame, text='Button_2', fg='blue')
-button_3 = Button(bottomFrame, text='Button_3', fg='green')
-button_4 = Button(bottomFrame, text='Button_4', fg='yellow')
 
-button_1.pack(side=LEFT)
-button_2.pack(side=RIGHT)
-button_3.pack(side=LEFT)
-button_4.pack(side=RIGHT)
+label_ = Label(text="Hey, I'm Jarvis, here to help you get around with you everyday tasks.😃", fg='black')
+label__= Label(text='Most Used:', fg='black')
+label_1 = Label(text="Check for updates on you Google Code In Dashboard.", fg='black')
+button_1 = Button(text='Dashboard', fg='orange',background='blue', activebackground='grey', command=openCodeIn)
+label_2 = Label(text="Manage files with Finder/Github Desktop", fg='black')
+button_2 = Button(text='Finder', fg='orange', background='blue', activebackground='grey', command=openFinder)
+button__2 = Button(text='GitH.Deskt', fg='orange', background='blue', activebackground='grey', command=openGitHubDesk)
+label_3 = Label(text='Tackle code with VsCode', fg='black')
+button_3 = Button(text='VsCode', fg='orange', bg='blue', activebackground='grey',command=openVsCode)
+
+label_.grid(row=0)
+label__.grid(row=2, column=0, sticky=W)
+label_1.grid(row=2, column=0, sticky=E)
+button_1.grid(row=2, column=1, sticky=W)
+label_2.grid(row=3, column=0, sticky=E)
+button_2.grid(row=3, column=1, sticky=W)
+button__2.grid(row=3, column=2, sticky=W)
+label_3.grid(row=4, column=0, sticky=E)
+button_3.grid(row=4, column=1, sticky=W)
+
 
 root.mainloop()
