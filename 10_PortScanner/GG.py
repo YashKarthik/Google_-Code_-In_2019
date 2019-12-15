4e3a9f02c5edd18ba2f33cd2d7496d3f7267ebc8
Yash <52369876+Yash10bloodstorm@users.noreply.github.com>
2019-12-15 17:47:53 +0530

    .

--- 10_PortScanner/GG.py
+++ 10_PortScanner/GG.py
@@ -1,5 +1,10 @@
-import socket    
-hostname = socket.gethostname()    
-IPAddr = socket.gethostbyname(hostname)    
-print("Your Computer's Name is:" + hostname)    
-print("Your Computer IP Address is:" + IPAddr)    
+import socket
+
+ip = input("Enter your ip address:   ")
+for port in range(1, 1025):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    result = sock.connect_ex((ip, port))
+    if 0 == result:
+        print("Port: {} Open".format(port))
+    sock.close()
--- 10_PortScanner/ip_PortScanner.py
+++ 10_PortScanner/ip_PortScanner.py
@@ -1,23 +1,40 @@
 import socket
+import requests
 
-hostname = socket.gethostname()    
-IPAddr = socket.gethostbyname(hostname)    
-print("Your Computer's Name is:" + hostname)    
-print("Your Computer IP Address is:" + IPAddr)    
+class PortScanner:
+    def __init__(self, hostname, IPAddr, ip, choice, result, port, sock, IP):
+        self.hostname = hostname
+        self.IPAddr = IPAddr
+        self.ip = ip
+        self.choice = choice
+        self.result = result
+        self.port = port
+        self.sock = sock
+        self.IP = IP
+        
 
-choice = input("Do you wanna scan a specific IP or your own(enter 'mine' for yours, enter 'specific' for scanning specific ip)")
+    hostname = socket.gethostname()    
+    IPAddr = socket.gethostbyname(hostname)    
+    ip = requests.get('https://checkip.amazonaws.com').text.strip()
+    print("Your Computer's Name is: " + hostname)    
+    print("Your Computer IP Address is: ", IPAddr)    
+    print("Your external IP address is: ", ip)
 
-if choice == 'mine':
-    ip = IPAddr
-elif choice == 'specific':
-    ip = input("Enter IP address:  ")
+    choice = input("Which do you wanna scan? (enter '1' for your computer's, enter '2' for your external IP addresss, enter '3' for scanning specific ip)  ")
+
+    if choice == 1:
+        IP = IPAddr
+    elif choice == 2:
+        IP = ip
+    elif choice == 3:
+        IP = input("Enter IP address:  ")
     
-for port in range(0, 65535):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    result = sock.connect_ex((ip, port))
-    if 0 == result:
-        print("Port: {} Open 😁😁".format(port))
-    else:
-        print("Port: {} CLOSED ☹️☹️".format(port), "----------------------")
-    sock.close()
+    for port in range(0, 65535):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((IP, port))
+        if 0 == result:
+            print("Port: {} Open 😁 😁".format(port))
+        else:
+            print("Port: {} CLOSED ☹️☹️".format(port), "----------------------")
+        sock.close()
