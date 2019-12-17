295b21eb2410d77f5b0ae65e19d9bd69e73b8d3e
Yash <52369876+Yash10bloodstorm@users.noreply.github.com>
2019-12-17 18:27:50 +0530

    Shodan Scanner done

--- 12_ShodanScanner/ShodanScanner.py
+++ 12_ShodanScanner/ShodanScanner.py
@@ -5,7 +5,7 @@ import socket
 api = shodan.Shodan('B37FoQIeHcfOarldXp7H7a2vzqZg5BrF')
 
 #Search the target by their IP address
-def Hostip():
+def HostIP():
     host_inp = input("Enter target's IP addrtess: ")
     host = api.host(host_inp)
 
@@ -23,7 +23,7 @@ Banner: {}
               """.format(item['port'], item['data']))
 
 #Search the target by their hostname
-def HostName():   
+def ShodanScan():   
     try:
         target = input("Enter the target's hostname: ")
         results = api.search(target)
@@ -45,4 +45,25 @@ def MyIP():
     except: 
         print("Unable to get Hostname and IP") 
 
-Hostip()
+choice = 1
+
+while choice != 0:
+    choice = int(input("""_________________________________
+Enter 1 - To get your IP.
+Enter 2 - To scan a specific IP address.
+Enter 3 - To Shodan scan using hostname.
+Enter 0 - To exit the script.
+
+INPUT: """))
+
+    if choice == 1:
+        MyIP()
+    elif choice == 2:
+        HostIP()
+    elif choice == 3:
+        ShodanScan()
+        
+if choice == 0:
+    print("-----------------EXITTING PROGRAM---------------")
+
+
